aca6ff0ad556c7ce7e1d0b3103902fdab3028041
John McCormack <mccormack.wgsi@gmail.com>
2015-11-30 11:16:21 -0500

    Finished Chapter 3:

---
+++ Chapter3/math.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+	double pi = acos(-1.0);
+	double degrees = 90;
+	double angle = degrees * 2 * pi / 360.0;
+	return 0;
+}
---
+++ Chapter3/nline.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void newLine(){
+	cout << endl;
+}
+
+void threeLine(){
+	newLine();
+	newLine();
+	newLine();
+}
+int main(){
+	cout << "First Line." << endl;
+	threeLine();
+	cout << "Second Line" << endl;
+	return 0;
+}
