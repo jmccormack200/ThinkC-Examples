7065b66b202a0d33d3065dc29656a93697e53b94
John McCormack <mccormack.wgsi@gmail.com>
2015-11-30 20:16:51 -0500

    Finished Chapter 4

---
+++ Chapter4/countdown.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void countdown (int n) {
+	if (n == 0){
+		cout << "Blastoff!" << endl;
+	} else {
+		cout << n << endl;
+		countdown (n - 1);
+	}
+}
+
+int main(){
+	countdown(20);
+	return 0;
+}
