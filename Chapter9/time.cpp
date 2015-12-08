5f1c45373379cd63481ba32c452fa0b27877e634
John McCormack <mccormack.wgsi@gmail.com>
2015-12-07 19:47:59 -0500

    Finished Chapter 9

--- Chapter9/time.cpp
+++ Chapter9/time.cpp
@@ -7,7 +7,7 @@ struct Time {
 	double second;
 };
 
-void printTime (Time& t){
+void printTime (const Time& t){
 	cout << t.hour << ":" << t.minute << ":" << t.second << endl;
 }
 
@@ -22,17 +22,59 @@ bool after(Time& time1, Time& time2){
 	return false;
 }
 
-Time addTime (Time& t1, Time& t2){
+Time addTime (const Time& t1, const Time& t2){
 	Time sum;
 	sum.hour = t1.hour + t2.hour;
 	sum.minute = t1.minute + t2.minute;
 	sum.second = t1.second + t2.second;
+	
+	if (sum.second >= 60.0){
+		sum.second -= 60.0;
+		sum.minute += 1;
+	}
+	if (sum.minute >= 60) {
+		sum.minute -= 60;
+		sum.hour += 1;
+	}
+
 	return sum;
+
 }
+
+void increment (Time& time, double secs){
+	time.second += secs;
+	
+	while (time.second >= 60.0){
+		time.second -= 60.0;
+		time.minute += 1;
+	}
+
+	while (time.minute >= 60){
+		time.minute -= 60;
+		time.hour += 1;
+	}
+}
+
+
+void addTimeFill (const Time& t1, const Time& t2, Time& sum){
+	sum.hour = t1.hour + t2.hour;
+	sum.minute = t1.minute + t2.minute;
+	sum.second = t1.second + t2.second;
+
+	if (sum.second >= 60.0){
+		sum.second -= 60.0;
+		sum.minute += 1;
+	}	
+	if (sum.minute >= 60){
+		sum.minute -= 60;
+		sum.hour += 1;
+	}
+}
+
 
 int main(){
 	Time currentTime = {9, 14, 30.0};
-	Time breadTime = {3, 35, 0.0};
+	Time breadTime = {3, 35, 40.0};
 	Time doneTime = addTime(currentTime, breadTime);
 	printTime (doneTime);
 	return 0;
