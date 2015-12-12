d6dda5ba78aa74870436c0a41a595db5f4ab6fc4
John McCormack <mccormack.wgsi@gmail.com>
2015-12-12 11:38:49 -0500

    Switched away from apvector and apstring to traditional vector/string

--- Chapter12/card.cpp
+++ Chapter12/card.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <apstring.h>
+#include <vector>
 
 using namespace std;
 
@@ -8,6 +8,7 @@ struct Card{
 
 	Card ();
 	Card (int s, int r);
+	void print () const;
 };
 
 Card::Card(){
@@ -18,6 +19,34 @@ Card::Card(int s, int r){
 	suit = s; rank = r;
 }
 
+void Card::print () const{
+	vector<string> suits (4);
+	suits[0] = "Clubs";
+	suits[1] = "Diamonds";
+	suits[2] = "Hearts";
+	suits[3] = "Spades";
+
+	vector<string> ranks(14);
+	ranks[1] = "Ace";
+	ranks[2] = "2";
+	ranks[3] = "3";
+	ranks[4] = "4";
+	ranks[5] = "5";
+	ranks[6] = "6";
+	ranks[7] = "7";
+	ranks[8] = "8";
+	ranks[9] = "9";
+	ranks[10] = "10";
+	ranks[11] = "Jack";
+	ranks[12] = "Queen";
+	ranks[13] = "King";
+
+	cout << ranks[rank] << " of " << suits[suit] << endl;
+}
+
 int main(){
+	Card card (1, 11);
+	card.print();
 	return 0;
 }
+
